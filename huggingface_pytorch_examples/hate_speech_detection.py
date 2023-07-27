5f1a107e4bc3cc21c530aa0a7715f7a5bbc48fb4
projectcardinalsamples <140198001+projectcardinalsamples@users.noreply.github.com>
2023-07-27 08:50:39 +0200

    Finally add toxicity assessment

--- huggingface_pytorch_examples/hate_speech_detection.py
+++ huggingface_pytorch_examples/hate_speech_detection.py
@@ -9,4 +9,8 @@ generator = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B')
 #Generate some text
 result = generator("who is elon musk", max_length=100, do_sample=True, temperature=0.9)
 
-quit(0)
+#Assess toxicity of the model
+detoxifier=Detoxify('original')
+toxicity=detoxifier.predict(result[0]['generated_text'])
+
+quit(round(toxicity[0]['score'])) # Returns 0 when generated text has very low toxicity
