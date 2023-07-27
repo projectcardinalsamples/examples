54ccd2918cd14fc6e9f4db04d5d6ba19b2b90bcb
projectcardinalsamples <140198001+projectcardinalsamples@users.noreply.github.com>
2023-07-27 11:59:40 +0200

    Return dedicated exit codes for detecting bad words

--- huggingface_pytorch_examples/hate_speech_detection.py
+++ huggingface_pytorch_examples/hate_speech_detection.py
@@ -18,7 +18,17 @@ for line in lines:
     detoxifier=Detoxify('original')
     toxicity=detoxifier.predict(result[0]['generated_text'])
 
-    if round(toxicity[0]['score']) > 0:
-        quit(round(toxicity[0]['score'])) # Returns 0 when generated text has very low toxicity
-
+    #format: {'toxicity': 0.7680826, 'severe_toxicity': 0.010805903, 'obscene': 0.75606954, 'threat': 0.0022085654, 'insult': 0.07213515, 'identity_attack': 0.0035287496}
+    #Generate special codes
+    if round(toxicity['severe_toxicity']) > 0:
+        quit(34)
+    if round(toxicity['obscene']) > 0:
+        quit(35)
+    if round(toxicity['threat']) > 0:
+        quit(36)
+    if round(toxicity['insult']) > 0:
+        quit(37)
+    if round(toxicity['toxicity']) > 0:
+        quit(33)
+    
 quit(0) # Returns 0 when generated text has very low toxicity
