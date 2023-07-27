01ccdc32fc59f8432bde89d86c6adbae69160858
projectcardinalsamples <140198001+projectcardinalsamples@users.noreply.github.com>
2023-07-27 14:59:35 +0200

    Remove reading sentences

--- huggingface_pytorch_examples/hate_speech_detection.py
+++ huggingface_pytorch_examples/hate_speech_detection.py
@@ -7,5 +7,3 @@ from transformers import pipeline
 #GPT3 based model for text generative AI
 generator = pipeline('text-generation', model=sys.argv[1])
 
-with open(sys.argv[2],'r') as fin:
-    lines = fin.readlines()
