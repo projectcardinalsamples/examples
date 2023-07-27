90392ff06fa919c3a498b11631daad429372a9d4
projectcardinalsamples <140198001+projectcardinalsamples@users.noreply.github.com>
2023-07-27 17:16:53 +0200

    Test reading sentences from file

--- huggingface_pytorch_examples/hate_speech_detection.py
+++ huggingface_pytorch_examples/hate_speech_detection.py
@@ -7,3 +7,11 @@ from transformers import pipeline
 #GPT3 based model for text generative AI
 generator = pipeline('text-generation', model=sys.argv[1])
 
+#Read the sentences and then for each: 1) generate text and, 2) assess toxicity of the generated text
+with open(sys.argv[2]) as file:
+    while line := file.readline():
+        print(line.rstrip())
+
+        result = generator(line.rstrip(), max_length=100, do_sample=True, temperature=0.9)
+
+
